afbc056656bdd06414d9a2824e4f884ae1290d8e
paxendacium <paxendacium@gmail.com>
2025-02-11 20:35:10 -0500

    fix

--- 3-ShellP1/starter/dshlib.c
+++ 3-ShellP1/starter/dshlib.c
@@ -18,42 +18,54 @@ char *trim_spaces(char *str)
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
     memset(clist, 0, sizeof(command_list_t));  
-    char *cmd_ptr = strtok(cmd_line, PIPE_STRING);
+    char *cmd_ptr = cmd_line;
     int cmd_count = 0;
 
     while (cmd_ptr != NULL)
     {
         if (cmd_count >= CMD_MAX)
         {
-            return ERR_TOO_MANY_COMMANDS;  
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        char *pipe_pos = strchr(cmd_ptr, PIPE_CHAR);
+        if (pipe_pos != NULL)
+        {
+            *pipe_pos = '\0';  
         }
 
         cmd_ptr = trim_spaces(cmd_ptr);
+        if (strlen(cmd_ptr) == 0)
+        {
+            cmd_ptr = (pipe_pos != NULL) ? pipe_pos + 1 : NULL;
+            continue;
+        }
 
         char *exe = strtok(cmd_ptr, " ");
         if (exe == NULL || strlen(exe) >= EXE_MAX)
         {
-            return ERR_CMD_OR_ARGS_TOO_BIG;  
+            return ERR_CMD_OR_ARGS_TOO_BIG;
         }
         strcpy(clist->commands[cmd_count].exe, exe);
 
         char *arg = strtok(NULL, "");
         if (arg != NULL)
         {
+            arg = trim_spaces(arg);
             if (strlen(arg) >= ARG_MAX)
             {
-                return ERR_CMD_OR_ARGS_TOO_BIG;  
+                return ERR_CMD_OR_ARGS_TOO_BIG;
             }
             strcpy(clist->commands[cmd_count].args, arg);
         }
 
         cmd_count++;
-        cmd_ptr = strtok(NULL, PIPE_STRING);  
+        cmd_ptr = (pipe_pos != NULL) ? pipe_pos + 1 : NULL;
     }
 
     if (cmd_count == 0)
     {
-        return WARN_NO_CMDS;  
+        return WARN_NO_CMDS;
     }
 
     clist->num = cmd_count;
